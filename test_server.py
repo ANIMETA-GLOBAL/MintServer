c6924741d10f6e9b0cf567da75abd13cf8c2735f
9th-Bacar <932581735@qq.com>
2022-08-01 14:23:47 +0800

    add test

---
+++ test_server.py
@@ -0,0 +1,16 @@
+import requests
+
+url = 'http://47.245.59.229:6666/mint'
+data = {'mint_request': {
+    "meta_data":
+        {
+            "name":"test nft",
+            "description":" test nft",
+            "image":"https://ipfs.io/ipfs/QmSWgjuqnKh4tApbHE8wfRoUSG9RWj6DX4NxPUJ2Q225M6?filename=5494c0fa4c8d21450ef7357d0929a5d8.jpegg"
+        },
+    "mint_amount":100
+}}
+
+x = requests.post(url, json=data)
+
+print(x.text)
