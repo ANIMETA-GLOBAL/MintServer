25db6bc5c5fbd16cd34612cff389cfc6ecbe6b24
9th-Bacar <932581735@qq.com>
2022-08-10 10:26:47 +0800

    Update animeta_ipfs.py

--- AnimetaIPFS/animeta_ipfs.py
+++ AnimetaIPFS/animeta_ipfs.py
@@ -1,5 +1,6 @@
 import ipfshttpclient
 import json
+import config
 
 dictionary = {
     "name": "sathiyajith",
@@ -8,10 +9,37 @@ dictionary = {
     "phonenumber": "9976770500"
 }
 
-json_object = json.dumps(dictionary, indent=4)
 
-with open("./metadata_temp/sample.json", "w") as outfile:
-    json.dump(dictionary, outfile)
-client = ipfshttpclient.connect()  # Connects to: /dns/localhost/tcp/5001/http
-res = client.add('./metadata_temp/sample.json')
-print(res)
+#
+# json_object = json.dumps(dictionary, indent=4)
+#
+# with open("./metadata_temp/sample.json", "w") as outfile:
+#     json.dump(dictionary, outfile)
+# client = ipfshttpclient.connect()  # Connects to: /dns/localhost/tcp/5001/http
+# res = client.add('./metadata_temp/sample.json')
+# print(res)
+
+
+class AnimetaIPFS(object):
+
+    def __init__(self):
+        self.client = ipfshttpclient.connect()
+
+    def upload(self, metadata):
+        with open(config.metadata_temp_dir + f"/{metadata['name']}.json", "w") as outfile:
+            json.dump(metadata, outfile)
+
+        res = self.client.add(config.metadata_temp_dir + f"/{metadata['name']}")
+
+        return res
+
+
+if __name__ == '__main__':
+    metadata = {
+        "name": "test",
+        "description": "test"
+    }
+
+    metadata_json = json.dumps(metadata)
+
+    print(AnimetaIPFS().upload(metadata))
