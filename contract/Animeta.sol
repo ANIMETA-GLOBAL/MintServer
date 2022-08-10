cc5b609af94287b59b7ccfaa735ebbf48ecd122c
9th-Bacar <932581735@qq.com>
2022-08-10 10:30:42 +0800

    add conrtact

---
+++ contract/Animeta.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract ANIMETA is ERC1155URIStorage, Ownable {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+    constructor() ERC1155("") {}
+
+    function mint(address account, uint256 amount, bytes memory data, string memory uri)
+        public
+        onlyOwner
+    {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _mint(account, tokenId, amount, data);
+        _setURI(tokenId,uri);
+    }
+
+    function setTokenUri(uint tokenId,string memory uri) public onlyOwner {
+        _setURI(tokenId,uri);
+    }
+
+}
