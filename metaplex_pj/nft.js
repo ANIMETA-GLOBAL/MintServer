befe242706354bd8df9bf87d01f96fdff65b25b4
9th-Bacar <932581735@qq.com>
2022-08-11 15:57:27 +0800

    add server

---
+++ metaplex_pj/nft.js
@@ -0,0 +1,56 @@
+import { Metaplex, keypairIdentity, bundlrStorage } from "@metaplex-foundation/js";
+import { Connection, clusterApiUrl, Keypair, PublicKey } from "@solana/web3.js";
+import { createClient } from 'redis';
+
+const client = createClient({
+    socket: {
+        host: "211.149.170.109",
+    },
+    password: "animeta"
+
+});
+const connection = new Connection(clusterApiUrl("devnet"));
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+await client.connect();
+
+const wallet = Keypair.generate();
+
+const metaplex = Metaplex.make(connection)
+    .use(keypairIdentity(wallet))
+    .use(bundlrStorage());
+
+
+async function findNft(ownerAddress) {
+    console.log()
+    const account = new PublicKey(ownerAddress);
+    const nft = await metaplex.nfts().findAllByOwner(account).run();
+    return nft
+}
+
+
+const subscriber = client.duplicate();
+await subscriber.connect();
+
+await subscriber.subscribe('rq_solana', (message) => {
+    // console.log(message); // 'message'
+    var request = JSON.parse(message);
+
+    try {
+
+        if (request.function == "getAllByOwner" && request.args.address) {
+            console.log("receipt getAllByOwner:");
+            console.log(request.args.address);
+            client.set(request.id,message);
+            findNft(request.args.address).then(res => {
+                console.log(res)
+                client.set(request.id, JSON.stringify(res))
+            })
+        }
+    } catch (error) {
+        client.set(request.id,error)
+    }
+
+});
+
--- test_server.py
+++ test_server.py
@@ -4,12 +4,13 @@ url = 'http://localhost:6666/mint'
 data = {'mint_request': {
     "meta_data":
         {
-            "name":"test nft",
-            "description":" test nft",
-            "image":"https://ipfs.io/ipfs/QmSWgjuqnKh4tApbHE8wfRoUSG9RWj6DX4NxPUJ2Q225M6?filename=5494c0fa4c8d21450ef7357d0929a5d8.jpegg"
+            "name": "test nft",
+            "description": " test nft",
+            "image": "https://ipfs.io/ipfs/QmSWgjuqnKh4tApbHE8wfRoUSG9RWj6DX4NxPUJ2Q225M6?filename=5494c0fa4c8d21450ef7357d0929a5d8.jpegg"
         },
-    "mint_amount":100,
-    "id":7948749
+    "mint_amount": 100,
+    "id": 7948749,
+    "network": "solana"
 }}
 
 x = requests.post(url, json=data)
